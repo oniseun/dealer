ae4aee11ba44df6c842c64a2c65c0174c405c8ce
Victor Oni <seunoni34@gmail.com>
2023-12-06 06:16:14 +0000

    chore: add swagger docs to start logs

--- src/server.js
+++ src/server.js
@@ -5,7 +5,9 @@ init();
 async function init() {
   try {
     app.listen(3001, () => {
-      console.log('Express App Listening on Port 3001');
+      console.warn(`Express App Listening on Port 3001`);
+      console.warn('=========================================================================\n')
+      console.warn('Swagger API Docs: http://localhost:3001/api-docs\n')
     });
   } catch (error) {
     console.error(`An error occurred: ${JSON.stringify(error)}`);
