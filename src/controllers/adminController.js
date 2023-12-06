67cbe1b254c03f722a274b5857f8b27317cc1ee7
Victor Oni <seunoni34@gmail.com>
2023-12-06 07:17:51 +0000

    refactor: validate input for admin controller

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,7 +1,17 @@
 const { getBestProfessionService, getBestClientsService } = require('../services/adminService');
 
+const isValidDate = (dateString) => {
+  const regex = /^\d{4}-\d{2}-\d{2}$/;
+  return regex.test(dateString) && !isNaN(Date.parse(dateString));
+};
+
 const getBestProfession = async (req, res) => {
   const { start, end } = req.query;
+
+  // Validate input
+  if (!start || !end || !isValidDate(start) || !isValidDate(end)) {
+    return res.status(400).json({ error: 'Invalid input. Both start and end dates must be valid.' });
+  }
 
   try {
     const bestProfession = await getBestProfessionService(start, end);
@@ -15,6 +25,11 @@ const getBestProfession = async (req, res) => {
 
 const getBestClients = async (req, res) => {
   const { start, end, limit } = req.query;
+
+  // Validate input
+  if (!start || !end || !isValidDate(start) || !isValidDate(end) || isNaN(Number(limit))) {
+    return res.status(400).json({ error: 'Invalid input. Both start and end dates must be valid, and limit must be a number.' });
+  }
 
   try {
     const bestClients = await getBestClientsService(start, end, limit);
