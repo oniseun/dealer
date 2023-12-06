396b54b5f8bf471203741523a82bec23ecfec706
Victor Oni <seunoni34@gmail.com>
2023-12-06 02:11:04 +0000

    chore: create new app routes

--- src/app.js
+++ src/app.js
@@ -1,21 +1,20 @@
+// src/app.js
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+const { sequelize } = require('./model');
+const contractRoutes = require('./routes/contractRoutes');
+const jobRoutes = require('./routes/jobRoutes');
+const balanceRoutes = require('./routes/balanceRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 const app = express();
-app.use(bodyParser.json());
-app.set('sequelize', sequelize)
-app.set('models', sequelize.models)
 
-/**
- * FIX ME!
- * @returns contract by id
- */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
-    res.json(contract)
-})
+app.use(bodyParser.json());
+app.set('sequelize', sequelize);
+app.set('models', sequelize.models);
+
+app.use('/contracts', contractRoutes);
+app.use('/jobs', jobRoutes);
+app.use('/balances', balanceRoutes);
+app.use('/admin', adminRoutes);
+
 module.exports = app;
