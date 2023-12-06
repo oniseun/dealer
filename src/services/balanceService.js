022d2df9deac2d0acbf3c7f30c5a82760a4769b8
Victor Oni <seunoni34@gmail.com>
2023-12-06 06:19:20 +0000

    fix: sequelize query for balance ops

--- src/services/balanceService.js
+++ src/services/balanceService.js
@@ -1,24 +1,47 @@
-const { Profile } = require('../models').models;
+const { Profile, Job, Contract } = require('../models').models;
 
 const depositMoneyService = async (userId, amount) => {
-  try {
-    // Ensure client can't deposit more than 25% of their total jobs to pay
-    const client = await Profile.findOne({ where: { id: userId, type: 'client' } });
-    const totalJobsToPay = await client.countJobs({ where: { paid: false } });
-    const maxDepositAmount = 0.25 * totalJobsToPay;
-
-    if (amount > maxDepositAmount) {
-      throw new Error(`Deposit exceeds 25% of total jobs to pay: ${maxDepositAmount}`);
+    try {
+      const client = await Profile.findOne({ where: { id: userId, type: 'client' } });
+  
+      if (!client) {
+        throw new Error(`Client with ID ${userId} not found`);
+      }
+  
+      const unpaidJobs = await Job.sum('price', {
+        where: {
+            paid: null
+        },
+        include: [
+            {
+                model: Contract,
+                where: { status: 'in_progress' },
+                include: [
+                    {
+                        model: Profile,
+                        as: 'Client',
+                        where: { id: userId }
+                    }
+                ]
+            }
+        ]
+    });
+  
+      const maxDepositAmount = 0.25 * unpaidJobs;
+  
+      if (amount > maxDepositAmount) {
+        throw new Error(`Deposit exceeds 25% of total jobs to pay: ${maxDepositAmount}`);
+      }
+  
+      // Update client's balance
+      await client.update({ balance: client.balance + amount });
+  
+      return { message: 'Deposit successful' };
+    } catch (error) {
+      console.error(error);
+      throw new Error('Error processing deposit');
     }
-
-    // Update client's balance
-    await client.update({ balance: client.balance + amount });
-
-    return { message: 'Deposit successful' };
-  } catch (error) {
-    console.error(error);
-    throw new Error('Error processing deposit');
-  }
-};
+  };
+  
 
 module.exports = { depositMoneyService };
