24bc5dbeaf25cd62890a56ec0e49e94da0aa4a26
Victor Oni <seunoni34@gmail.com>
2023-12-06 06:18:57 +0000

    fix: sequelize query for admin ops

--- src/services/adminService.js
+++ src/services/adminService.js
@@ -1,58 +1,75 @@
-const { Profile, Job, Contract } = require('../models').models;
+const { models: { Profile, Job, Contract } , sequelize } = require('../models');
 const { Op } = require('sequelize');
 
 const getBestProfessionService = async (start, end) => {
-  try {
-    const bestProfession = await Profile.findAll({
-      attributes: ['profession', [sequelize.fn('sum', sequelize.col('Jobs.price')), 'totalEarnings']],
-      include: [
-        {
-          model: Contract,
-          where: {
-            status: 'terminated',
-            paymentDate: { [Op.between]: [start, end] },
+    try {
+      const bestProfession = await Job.findAll({
+        attributes: [[sequelize.fn('sum', sequelize.col('price')), 'total']],
+        limit: 1,
+        group: ['Contract.Contractor.profession'],
+        order: [[sequelize.fn('sum', sequelize.col('price')), 'DESC']],
+        include: [
+          {
+            attributes: ['createdAt'],
+            include: [{
+              attributes: ['profession'], as: 'Contractor', model: Profile, where: { type: 'contractor' },
+            }],
+            model: Contract,
           },
-          include: [Job],
+        ],
+        where: { paid: true, createdAt: { [Op.between]: [start, end] } },
+      });
+  
+      if (!bestProfession || bestProfession.length === 0) {
+        throw new Error('No data found');
+      }
+      
+        const best = bestProfession.shift().get({ plain: true });
+
+      return {
+        profession: best.Contract.Contractor.profession,
+        totalEarnings: best.total,
+      };
+    } catch (error) {
+      console.error(error);
+      throw new Error('Error retrieving best profession');
+    }
+  };
+
+
+  const getBestClientsService = async (start, end, limit) => {
+    try {
+      const bestClients = await Job.findAll({
+        where: {
+          paid: 1,
+          "paymentDate": {[Op.between] : [start , end]} 
         },
-      ],
-      group: ['Profile.profession'],
-      order: [[sequelize.fn('sum', sequelize.col('Jobs.price')), 'DESC']],
-      limit: 1,
-    });
-
-    if (!bestProfession || bestProfession.length === 0) throw new Error('No data found');
-
-    return { profession: bestProfession[0].profession, totalEarnings: bestProfession[0].totalEarnings };
-  } catch (error) {
-    console.error(error);
-    throw new Error('Error retrieving best profession');
-  }
-};
-
-const getBestClientsService = async (start, end, limit) => {
-  try {
-    const bestClients = await Profile.findAll({
-      attributes: ['id', 'fullName', [sequelize.fn('sum', sequelize.col('Jobs.price')), 'paid']],
-      include: [
-        {
+        attributes: [
+            "Contract.Client.id",
+            [sequelize.literal("firstName || ' ' || lastName"), 'fullName'],
+            [sequelize.fn('sum', sequelize.col('price')), 'paid'],
+        ],
+        order: [[sequelize.col('paid'), 'DESC']],
+        include: [{
           model: Contract,
-          where: {
-            status: 'terminated',
-            paymentDate: { [Op.between]: [start, end] },
-          },
-          include: [Job],
-        },
-      ],
-      group: ['Profile.id'],
-      order: [[sequelize.fn('sum', sequelize.col('Jobs.price')), 'DESC']],
-      limit: parseInt(limit),
-    });
-
-    return bestClients;
-  } catch (error) {
-    console.error(error);
-    throw new Error('Error retrieving best clients');
-  }
-};
+          attributes: {exclude: ['id', 'terms', 'status', 'ContractorId', 'ClientId','createdAt', 'updatedAt']},
+          include: [{
+            model: Profile,
+            as: 'Client',
+            attributes: {exclude: ['id', 'firstName', 'lastName', 'profession', 'balance', 'type', 'createdAt', 'updatedAt']},
+            where: { type: 'client'}
+          }],
+        }],
+        group: ["Contract->Client.firstName"],
+        limit: parseInt(limit),
+        raw: true,
+      })
+  
+      return bestClients;
+    } catch (error) {
+      console.error(error);
+      throw new Error('Error retrieving best clients');
+    }
+  };
 
 module.exports = { getBestProfessionService, getBestClientsService };
