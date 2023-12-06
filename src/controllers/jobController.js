c618423c727635abf9a0d971063e4044ca5992ef
Victor Oni <seunoni34@gmail.com>
2023-12-06 01:55:15 +0000

    feat: create job controller for routing

---
+++ src/controllers/jobController.js
@@ -0,0 +1,27 @@
+const { getUnpaidJobsService, payForJobService } = require('../services/jobService');
+
+const getUnpaidJobs = async (req, res) => {
+  try {
+    const unpaidJobs = await getUnpaidJobsService(req.profile.id);
+
+    res.json(unpaidJobs);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+const payForJob = async (req, res) => {
+  const { job_id } = req.params;
+
+  try {
+    await payForJobService(job_id);
+
+    res.json({ message: 'Payment successful' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+module.exports = { getUnpaidJobs, payForJob };
