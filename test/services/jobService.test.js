d8975a95e457db5ca5b67956d9a88bbfb0184fd4
Victor Oni <seunoni34@gmail.com>
2023-12-06 06:20:00 +0000

    test: add test for job ops fix

--- test/services/jobService.test.js
+++ test/services/jobService.test.js
@@ -1,100 +1,182 @@
-const chai = require('chai');
-const sinon = require('sinon');
-const jobService = require('../../src/services/jobService');
-const { Contract, Job, Profile } = require('../../src/models').models;
+const { getUnpaidJobsService, payForJobService } = require('./jobService');
+const { models: { Job, Contract, Profile }, sequelize } = require('../models');
 
-const expect = chai.expect;
+// Mock Sequelize functions
+jest.mock('../models', () => {
+  const SequelizeMock = require('sequelize-mock');
+  const dbMock = new SequelizeMock();
 
-describe('Job Service Tests', () => {
-  describe('getUnpaidJobsService', () => {
-    it('should get unpaid jobs for a user', async () => {
-      const fakeUnpaidJobs = [{ id: 1, description: 'Sample job', price: 100, paid: false }];
-      sinon.stub(Profile, 'findOne').resolves({ id: 1, type: 'client' });
-      sinon.stub(Contract, 'findAll').resolves([{ id: 1 }]);
-      sinon.stub(Job, 'findAll').resolves(fakeUnpaidJobs);
+  const ProfileMock = dbMock.define('Profile', {
+    id: 1,
+    balance: 100.0,
+  });
 
-      const result = await jobService.getUnpaidJobsService(1);
+  const ContractMock = dbMock.define('Contract', {
+    id: 1,
+    status: 'in_progress',
+  });
 
-      expect(result).to.deep.equal(fakeUnpaidJobs);
+  const JobMock = dbMock.define('Job', {
+    id: 1,
+    paid: false,
+    price: 20.0,
+  });
 
-      Profile.findOne.restore();
-      Contract.findAll.restore();
-      Job.findAll.restore();
-    });
+  // Associations
+  ProfileMock.hasMany(ContractMock, { foreignKey: 'ClientId' });
+  ContractMock.belongsTo(ProfileMock, { as: 'Client' });
+  ProfileMock.hasMany(ContractMock, { foreignKey: 'ContractorId' });
+  ContractMock.belongsTo(ProfileMock, { as: 'Contractor' });
+  ContractMock.hasMany(JobMock);
+  JobMock.belongsTo(ContractMock);
 
-    it('should throw an error on database error', async () => {
-      sinon.stub(Profile, 'findOne').resolves({ id: 1, type: 'client' });
-      sinon.stub(Contract, 'findAll').resolves([{ id: 1 }]);
-      sinon.stub(Job, 'findAll').throws(new Error('Test Error'));
+  return {
+    models: {
+      Job: JobMock,
+      Contract: ContractMock,
+      Profile: ProfileMock,
+    },
+    sequelize: dbMock,
+  };
+});
 
-      try {
-        await jobService.getUnpaidJobsService(1);
-      } catch (error) {
-        expect(error.message).to.equal('Test Error');
-      }
+describe('getUnpaidJobsService', () => {
+  it('should retrieve unpaid jobs for a profile', async () => {
+    const profileId = 1;
 
-      Profile.findOne.restore();
-      Contract.findAll.restore();
-      Job.findAll.restore();
+    // Mock data
+    const unpaidJobsData = [
+      {
+        id: 1,
+        paid: false,
+        price: 20.0,
+        Contract: {
+          id: 1,
+          status: 'in_progress',
+        },
+      },
+    ];
+
+    // Mock Sequelize findAll function
+    Job.findAll = jest.fn().mockResolvedValue(unpaidJobsData);
+
+    const unpaidJobs = await getUnpaidJobsService(profileId);
+
+    expect(unpaidJobs).toEqual(unpaidJobsData);
+    expect(Job.findAll).toHaveBeenCalledWith({
+      include: [
+        {
+          model: Contract,
+          where: {
+            [sequelize.Op.or]: [{ ContractorId: profileId }, { ClientId: profileId }],
+            status: 'in_progress',
+          },
+        },
+      ],
+      where: {
+        [sequelize.Op.or]: [{ paid: false }, { paid: { [sequelize.Op.is]: null } }],
+      },
     });
   });
 
-  describe('payForJobService', () => {
-    it('should pay for a job', async () => {
-      const fakeJob = { id: 1, description: 'Sample job', price: 100, paid: false };
-      sinon.stub(Profile, 'findOne').resolves({ id: 1, type: 'client' });
-      sinon.stub(Job, 'findOne').resolves(fakeJob);
+  it('should handle errors during retrieval', async () => {
+    const profileId = 1;
 
-      const result = await jobService.payForJobService(1, 1);
+    // Mock Sequelize findAll function to throw an error
+    Job.findAll = jest.fn().mockRejectedValue(new Error('Database error'));
 
-      expect(result.message).to.equal('Payment successful');
+    await expect(getUnpaidJobsService(profileId)).rejects.toThrow('Error retrieving unpaid jobs');
+  });
+});
 
-      Profile.findOne.restore();
-      Job.findOne.restore();
+describe('payForJobService', () => {
+  it('should process job payment successfully', async () => {
+    const clientId = 1;
+    const jobId = 1;
+
+    // Mock data
+    const jobData = {
+      id: jobId,
+      paid: false,
+      price: 20.0,
+      Contract: {
+        id: 1,
+        Contractor: {
+          id: 2,
+          balance: 80.0,
+        },
+      },
+    };
+
+    // Mock Sequelize findOne and update functions
+    Job.findOne = jest.fn().mockResolvedValue(jobData);
+    Profile.findOne = jest.fn().mockResolvedValue({
+      id: clientId,
+      balance: 50.0,
+      update: jest.fn(),
     });
 
-    it('should handle insufficient balance', async () => {
-      const fakeJob = { id: 1, description: 'Sample job', price: 100, paid: false };
-      sinon.stub(Profile, 'findOne').resolves({ id: 1, type: 'client', balance: 50 });
-      sinon.stub(Job, 'findOne').resolves(fakeJob);
+    // Mock Sequelize transaction functions
+    sequelize.transaction = jest.fn().mockImplementation((callback) => callback());
 
-      try {
-        await jobService.payForJobService(1, 1);
-      } catch (error) {
-        expect(error.message).to.equal('Insufficient balance');
-      }
+    const paymentResult = await payForJobService(clientId, jobId);
 
-      Profile.findOne.restore();
-      Job.findOne.restore();
+    expect(paymentResult).toEqual({ message: 'Payment successful' });
+    expect(Job.findOne).toHaveBeenCalledWith({
+      where: {
+        id: jobId,
+        paid: {
+          [sequelize.Op.or]: [false, null],
+        },
+      },
+      include: [
+        {
+          model: Contract,
+          include: [{ model: Profile, as: 'Contractor' }],
+        },
+      ],
     });
-
-    it('should handle job already paid', async () => {
-      const fakeJob = { id: 1, description: 'Sample job', price: 100, paid: true };
-      sinon.stub(Profile, 'findOne').resolves({ id: 1, type: 'client', balance: 150 });
-      sinon.stub(Job, 'findOne').resolves(fakeJob);
-
-      try {
-        await jobService.payForJobService(1, 1);
-      } catch (error) {
-        expect(error.message).to.equal('Job already paid');
-      }
-
-      Profile.findOne.restore();
-      Job.findOne.restore();
+    expect(Profile.findOne).toHaveBeenCalledWith({ where: { id: clientId } });
+    expect(sequelize.transaction).toHaveBeenCalled();
+    expect(Profile.findOne().update).toHaveBeenCalledWith({ balance: 30.0 });
+    expect(Profile.findOne().update).toHaveBeenCalledWith({ balance: 100.0 });
+    expect(Job.findOne().update).toHaveBeenCalledWith({
+      paid: true,
+      paymentDate: expect.any(Date),
     });
+  });
 
-    it('should throw an error on database error', async () => {
-      sinon.stub(Profile, 'findOne').resolves({ id: 1, type: 'client', balance: 150 });
-      sinon.stub(Job, 'findOne').throws(new Error('Test Error'));
+  it('should handle insufficient funds', async () => {
+    const clientId = 1;
+    const jobId = 1;
 
-      try {
-        await jobService.payForJobService(1, 1);
-      } catch (error) {
-        expect(error.message).to.equal('Test Error');
-      }
+    // Mock data with insufficient funds
+    const jobData = {
+      id: jobId,
+      paid: false,
+      price: 80.0, // Job price exceeds client's balance
+      Contract: {
+        id: 1,
+        Contractor: {
+          id: 2,
+          balance: 80.0,
+        },
+      },
+    };
 
-      Profile.findOne.restore();
-      Job.findOne.restore();
-    });
+    // Mock Sequelize findOne function
+    Job.findOne = jest.fn().mockResolvedValue(jobData);
+
+    await expect(payForJobService(clientId, jobId)).rejects.toThrow('Insufficient funds');
+  });
+
+  it('should handle errors during payment processing', async () => {
+    const clientId = 1;
+    const jobId = 1;
+
+    // Mock Sequelize findOne function to throw an error
+    Job.findOne = jest.fn().mockRejectedValue(new Error('Database error'));
+
+    await expect(payForJobService(clientId, jobId)).rejects.toThrow('Error processing job payment');
   });
 });
