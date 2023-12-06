94c9fd4cef18540cd074d8e1c0bceb74a6cc7d4b
Victor Oni <seunoni34@gmail.com>
2023-12-06 06:19:40 +0000

    fix: sequelize query for contract ops

--- src/services/contractService.js
+++ src/services/contractService.js
@@ -1,35 +1,59 @@
 const { Contract, Profile } = require('../models').models;
+const { Op } = require('sequelize');
 
 const getContractByIdService = async (profileId, contractId) => {
-  try {
-    const contract = await Contract.findOne({
-      where: { id: contractId, [Contract.belongsTo(Profile, { as: 'Client' }).foreignKey]: profileId },
-    });
-
-    return contract;
-  } catch (error) {
-    console.error(error);
-    throw new Error('Error retrieving contract by ID');
-  }
-};
-
-const getContractsService = async (profileId) => {
-  try {
-    const contracts = await Contract.findAll({
-      where: {
-        [Op.or]: [
-          { [Contract.belongsTo(Profile, { as: 'Client' }).foreignKey]: profileId },
-          { [Contract.belongsTo(Profile, { as: 'Contractor' }).foreignKey]: profileId },
+    try {
+      const contract = await Contract.findOne({
+        include: [
+          {
+            model: Profile,
+            as: 'Client',
+            attributes: [],
+          },
         ],
-        status: { [Op.ne]: 'terminated' },
-      },
-    });
-
-    return contracts;
-  } catch (error) {
-    console.error(error);
-    throw new Error('Error retrieving contracts');
-  }
-};
+        where: {
+          id: contractId,
+          '$Client.id$': profileId,
+        },
+      });
+  
+      return contract;
+    } catch (error) {
+      console.error(error);
+      throw new Error('Error retrieving contract by ID');
+    }
+  };
+  
+const getContractsService = async (profileId) => {
+    try {
+      const contracts = await Contract.findAll({
+        include: [
+          {
+            model: Profile,
+            as: 'Client',
+            attributes: [],
+          },
+          {
+            model: Profile,
+            as: 'Contractor',
+            attributes: [],
+          },
+        ],
+        where: {
+          [Op.or]: [
+            { '$Client.id$': profileId },
+            { '$Contractor.id$': profileId },
+          ],
+          status: { [Op.ne]: 'terminated' },
+        },
+      });
+  
+      return contracts;
+    } catch (error) {
+      console.error(error);
+      throw new Error('Error retrieving contracts');
+    }
+  };
+  
 
 module.exports = { getContractByIdService, getContractsService };
